ad4c16885d005813951386ad342131f4a4d02936
Katerina2010dev <Katerina2simonova@gmail.com>
2026-01-12 10:05:44 +0200

    MethodCall

---
+++ MethodCall/MethodCall/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saad valida enda meetodi");
+            Console.WriteLine("1. For loop");
+            Console.WriteLine("2. While loop");
+            Console.WriteLine("3. Liitmine");
+            Console.WriteLine("4. Nime küsimine");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                ForLoop();
+            }
+            else if (choice == "2")
+            {
+                WhileLoop();
+            }
+            else if (choice == "3")
+            {
+                AddTwoNumbers();
+            }
+            else if (choice == "4")
+            {
+                AskName();
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+
+
+        static void ForLoop()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Tere tulemast for loop");
+            }
+        }
+
+        static void WhileLoop()
+        {
+            int number = 0;
+
+            do
+            {
+                Console.WriteLine("Tere tulemast for loop");
+                number++;
+
+            }
+            while (number > 0);
+
+        }
+
+        static void AddTwoNumbers()
+        {
+            Console.WriteLine("Numbrite liitmine");
+            Console.WriteLine("Sisesta esimene number");
+            int firstNr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisesta teine number");
+            int secondNr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vastus: " + (firstNr + secondNr));
+        }
+
+        static void AskName()
+        {
+            Console.WriteLine("Sisesta enda nimi");
+            string yourAge = Console.ReadLine();
+            Console.WriteLine("Sinu vanus on " + yourAge);
+        }
+    }
+}
+
+
+
---
+++ Tunnitöö/Tunnitöö/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Tunnitöö
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere tulemast!");
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("tere tulemast " + i);
+
+                int a = 1;
+
+                while (a <= 3)
+                {
+                    //uus while
+                    int b = 1;
+                    while (b <= 3)
+                    {
+                        Console.Write(b + " ");
+                        b++;
+                    }
+
+                    Console.WriteLine();
+                    a++;
+
+                    Console.WriteLine("Sisesta esimene number");
+                    float firstNumber = float.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Sisesta tehe");
+                    string operation = Console.ReadLine();
+
+                    Console.WriteLine("Sisesta teine number");
+                    float secondNumber = float.Parse(Console.ReadLine());
+
+
+                    switch (operation)
+                    {
+                        case "+":
+                            Console.WriteLine($"Tulemus: {firstNumber + secondNumber}");
+                            break;
+
+                        case "-":
+                            Console.WriteLine($"Tulemus: {firstNumber - secondNumber}");
+                            break;
+
+                        case "*":
+                            Console.WriteLine($"Tulemus: {firstNumber * secondNumber}");
+                            break;
+
+                        case "/":
+                            if (secondNumber == 0)
+                            {
+                                Console.WriteLine("Nulliga jagamine ei ole lubatud!");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Tulemus: {firstNumber / secondNumber}");
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine("Tundmatu tehe!");
+                            break;
+
+                    }
+                }
+            }
+        }
+    }
+}
